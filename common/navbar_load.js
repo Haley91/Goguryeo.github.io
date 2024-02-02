95ab1dc4a362a4be247481a16e9b3e3d59ec89ff
Bataan <158548846+Haley91@users.noreply.github.com>
2024-02-02 15:36:05 +0300

    Delete common directory

--- common/navbar_load.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function() {
-    // navbar load
-    $("#navbar").load("https://14desu.github.io/common/navbar.html");
-    // sidebar load
-    $("#sidenavAccordion").load("https://14desu.github.io/common/sidebar.html");
-});
